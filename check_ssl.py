d6df6e5cddfc7e90334e0243ef1f5ee519050b04
Samuel Mellert <samuel.mellert@gmail.com>
2024-06-05 07:28:29 +0200

    Added explicit support for STARTTLS
    - use STARTTLS on ports: 25, 110, 143, 587

--- check_ssl.py
+++ check_ssl.py
@@ -3,19 +3,28 @@ import socket
 import datetime
 import requests
 from dotenv import load_dotenv
+import smtplib
 import os
 import time
 
 load_dotenv()
 
-def get_certificate_expiry(host, port):
+def get_certificate_expiry(host, port, use_starttls=False):
     context = ssl.create_default_context()
-    with socket.create_connection((host, port)) as sock:
-        with context.wrap_socket(sock, server_hostname=host) as ssock:
-            cert = ssock.getpeercert()
-            expiry = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
-            expiry = expiry.replace(tzinfo=datetime.timezone.utc)
-            return expiry
+    if use_starttls:
+        with smtplib.SMTP(host, port) as server:
+            server.ehlo()
+            server.starttls(context=context)
+            server.ehlo()
+            cert = server.sock.getpeercert()
+    else:
+        with socket.create_connection((host, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=host) as ssock:
+                cert = ssock.getpeercert()
+    
+    expiry = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+    expiry = expiry.replace(tzinfo=datetime.timezone.utc)
+    return expiry
 
 def send_to_uptime_kuma(url, status, msg):
     params = {
@@ -30,9 +39,12 @@ minimum_validity_days = int(os.getenv("MINIMUM_VALIDITY_DAYS", 30))
 uptime_kuma_push_url = os.getenv("UPTIME_KUMA_PUSH_URL")
 heartbeat_interval = int(os.getenv("HEARTBEAT_INTERVAL", 0))
 
+# Determine if STARTTLS is needed based on the port number
+use_starttls = port in {25, 110, 143, 587}
+
 while True:    
     try:
-        expiry = get_certificate_expiry(host, port)
+        expiry = get_certificate_expiry(host, port, use_starttls=use_starttls)
         now = datetime.datetime.now(datetime.timezone.utc)
         days_left = (expiry - now).days
         msg = f"SSL Expiry for {host}:{port} is in {days_left} days"
